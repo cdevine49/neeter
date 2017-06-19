a7f495b6a782fc1c07f8c163bf6ca29d28f85f49
Conor Devine <conorjdevine@gmail.com>
2017-06-19 16:21:11 -0400

    set up html root

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongo = require('mongodb');
 const mongoose = require('mongoose');
 const sessions = require('express-session');
+const path = require('path');
 
 const controllers = require('./controllers');
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/neeter')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname)));
 
 app.use(sessions({
   secret: 'ajsldjf093ujrgj9824asdfjLFJjlsdfj32094SA',
@@ -19,11 +21,7 @@ app.use(sessions({
 }))
 
 app.get('/', function(req, res, next) {
-  if (req.session.user) {
-    res.send('Neeter');
-  } else {
-    res.status(401).send("Requires Login");
-  }
+  res.sendFile(path.join(__dirname+'/views/index.html'));
 });
 
 controllers(app);
